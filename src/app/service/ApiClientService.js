fe457e91c0d08283bcc2021155342b9bc0de1934
dougllas sousa <dougllasfps@gmail.com>
2019-01-29 11:44:21 -0300

    Adicionando cliente da API

--- src/app/service/ApiClientService.js
+++ src/app/service/ApiClientService.js
@@ -1,6 +1,28 @@
 import axios from './axios'
 
 export default class ApiClientService{
+
+    constructor(apiUrl){
+        this.apiUrl = apiUrl;
+    }
+
+    get = async (query) => {
+        return await axios.get(`${this.apiUrl}${query}`)
+    }
+
+    post = async (object) => {
+        return await axios.post(this.apiUrl, object)
+    }
+
+    put = async (object) => {
+        const {id} = object
+        return await axios.put(`${this.apiUrl}/${id}`, object)
+    }
+
+    delete = async (id) => {
+        return await axios.delete(`${this.apiUrl}/${id}`)
+    }
+
     static setToken( token ){
         axios.default.headers.commom['Authorization'] = `Bearer ${token}`
     }
